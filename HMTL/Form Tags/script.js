b5c8b71d284afc5722f1a91a342586a095b4f3da
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-01-19 19:00:33 +0530

    HTML Files Practiced

---
+++ HMTL/Form Tags/script.js
@@ -0,0 +1,13 @@
+// document.write(window);
+// console.log(window);
+
+
+// prompt("What is your name?");
+// document.write(prompt("What is your name?"));
+
+
+// var fname= "mohammed sharuk ms";
+// alert(fname);
+
+// var a = "Mohammed sharuk m s" ;
+// confirm("Are you sure you want to delete this file?" + a);
