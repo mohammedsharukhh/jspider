7b1374f57e4620d2273a8246ae3d6392e537a33a
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-01-20 16:04:47 +0000

    practiced concepts on type conversion in implisit

--- Jspiders Class/javacript/app.js
+++ Jspiders Class/javacript/app.js
@@ -5,3 +5,35 @@ document.write(typeof null);
 document.write(typeof String);
 document.write(typeof 2 + Boolean());
 document.write(typeof Boolean());
+
+// practice questions
+
+document.write(2+2);
+document.writeln(typeof 2 +"2");
+document.write(typeof 1 * "2");
+document.write(typeof true + 2);
+document.write(typeof true * 2);
+document.write(2-2);
+document.write(typeof 2 - "2");
+document.write(typeof false - 2);
+document.write(typeof true + 2);
+document.write(typeof "2" - 4);
+ document.write(typeof 0 + 1);
+document.write(typeof null + null);
+document.write(typeof null - 45);
+document.write(typeof null + "3");
+document.write(typeof null + 5);
+document.write(46 + "54");
+document.write(31 - "hii");
+document.write(31 - "02");
+document.write(5 * "2");
+document.write("2" / 2);
+document.write(23 + "23");
+document.write(10 + "hii");
+document.write(typeof Boolean());
+document.write(typeof 2 - 2);
+document.write(typeof NaN);
+let a = "hii";
+document.write(isNaN(a)); 
+a = 10;
+document.write(isNaN(a));
