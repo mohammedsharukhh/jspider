4a9c065f6f6d0e36d7894d7830faf77583ddc108
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-01-20 07:17:42 +0000

    javascript class

---
+++ Jspiders Class/javacript/app.js
@@ -0,0 +1,7 @@
+document.write(typeof 2+3);
+document.write(typeof Boolean);
+document.write(typeof Symbol());
+document.write(typeof null);
+document.write(typeof String);
+document.write(typeof 2 + Boolean());
+document.write(typeof Boolean());
