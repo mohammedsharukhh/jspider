90ab7727f16d6a98a9a51c22633d3d3789175e8e
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-01-24 08:21:35 +0000

    javascript Problems solved

--- Jspiders Class/javacript/app.js
+++ Jspiders Class/javacript/app.js
@@ -1,114 +1,129 @@
- document.write(typeof 2+3);
- document.write(typeof Boolean);
- document.write(typeof Symbol());
- document.write(typeof null);
- document.write(typeof String);
- document.write(typeof 2 + Boolean());
- document.write(typeof Boolean());
+//  document.write(typeof 2+3);
+//  document.write(typeof Boolean);
+//  document.write(typeof Symbol());
+//  document.write(typeof null);
+//  document.write(typeof String);
+//  document.write(typeof 2 + Boolean());
+//  document.write(typeof Boolean());
 
-  practice questions
+//   practice questions
 
- document.write(2+2);
- document.writeln(typeof 2 - 2);
- document.write(typeof 1 * "2");
- document.write(typeof true + 2);
- document.write(typeof true * 2);
- document.write(2-2);
- document.write(typeof 2 - "2");
- document.write(typeof false - 2);
- document.write(typeof true + 2);
- document.write(typeof "2" - 4);
-  document.write(typeof 0 + 1);
- document.write(null);
- document.write(typeof null - 45);
- document.write(typeof null + "3");
- document.write(typeof null + 5);
- document.write(46 + "54");
- document.write(31 - "hii");
- document.write(31 - "02");
- document.write(5 * "2");
- document.write(typeof "/" + /);
- document.write(23 + "23");
- document.write(/10 + "hii");
- document.write(typeof Boolean());
- document.write(typeof 2 - 2);
- document.write(typeof NaN);
- let a = "hii";
- document.write(isNaN(a)); 
- a = 10;
- document.write(isNaN(a));
- document.write(typeof NaN);
+//  document.write(2+2);
+//  document.writeln(typeof 2 - 2);
+//  document.write(typeof 1 * "2");
+//  document.write(typeof true + 2);
+//  document.write(typeof true * 2);
+//  document.write(2-2);
+//  document.write(typeof 2 - "2");
+//  document.write(typeof false - 2);
+//  document.write(typeof true + 2);
+//  document.write(typeof "2" - 4);
+//   document.write(typeof 0 + 1);
+//  document.write(null);
+//  document.write(typeof null - 45);
+//  document.write(typeof null + "3");
+//  document.write(typeof null + 5);
+//  document.write(46 + "54");
+//  document.write(31 - "hii");
+//  document.write(31 - "02");
+//  document.write(5 * "2");
+//  document.write(typeof "/" + /);
+//  document.write(23 + "23");
+//  document.write(/10 + "hii");
+//  document.write(typeof Boolean());
+//  document.write(typeof 2 - 2);
+//  document.write(typeof NaN);
+//  let a = "hii";
+//  document.write(isNaN(a)); 
+//  a = 10;
+//  document.write(isNaN(a));
+//  document.write(typeof NaN);
 
- console.log("Hello World");
- document.write("Hello World");
- confirm("Are you sure?");
- alert("Hello World");
- prompt("Enter your name");
+//  console.log("Hello World");
+//  document.write("Hello World");
+//  confirm("Are you sure?");
+//  alert("Hello World");
+//  prompt("Enter your name");
 
- let a = 10;
- document.write(typeof a);
- a = "hii";
- document.write(typeof a);
- a = true;
- document.write(typeof a);
- let a= null;
- document.write(typeof a);
- a = undefined;
- document.write(typeof a);
- a = Symbol();
- document.write(typeof a);
- a = 12784812964172461729437465456547567567246n;
- document.write(typeof a);
- a = 10.5;
- document.write(typeof a);
+//  let a = 10;
+//  document.write(typeof a);
+//  a = "hii";
+//  document.write(typeof a);
+//  a = true;
+//  document.write(typeof a);
+//  let a= null;
+//  document.write(typeof a);
+//  a = undefined;
+//  document.write(typeof a);
+//  a = Symbol();
+//  document.write(typeof a);
+//  a = 12784812964172461729437465456547567567246n;
+//  document.write(typeof a);
+//  a = 10.5;
+//  document.write(typeof a);
 
- var a = "hii";
- console.log(a);
- a[0] = "bye";
- console.log(a);
+//  var a = "hii";
+//  console.log(a);
+//  a[0] = "bye";
+//  console.log(a);
 
- var a = [10,20,30,40,50];
- console.log(a);
- a[0] = 100;
- console.log(a); 
-
-
- console.log(a);
- var a = 10;
- console.log(a);
-
- console.log(a);
- let a = 10;
- console.log(a);
-
- document.write(1+2);
- document.write(1-"2");
-
- let number1 = 10(prompt("Please enter a number"));
- let number2 = Number(prompt("Please enter another number"));
- document.write(`The sum of ${number1} and ${number2} is ${number1 + number2}`);
-
- var a=10;
- var b=20;
- document.write(a+b);
- document.write(a);
+//  var a = [10,20,30,40,50];
+//  console.log(a);
+//  a[0] = 100;
+//  console.log(a); 
 
 
-golbal scope;
-script scope;
-block scope;
-local scope/ function scope;
+//  console.log(a);
+//  var a = 10;
+//  console.log(a);
+
+//  console.log(a);
+//  let a = 10;
+//  console.log(a);
+
+//  document.write(1+2);
+//  document.write(1-"2");
+
+//  let number1 = 10(prompt("Please enter a number"));
+//  let number2 = Number(prompt("Please enter another number"));
+//  document.write(`The sum of ${number1} and ${number2} is ${number1 + number2}`);
+
+//  var a=10;
+//  var b=20;
+//  document.write(a+b);
+//  document.write(a);
 
 
- golbal scope 
+// golbal scope;
+// script scope;
+// block scope;
+// local scope/ function scope;
 
- any identifer is declared using by using var keywork can ba accessed anywhere in the progeram;
- var b = 20;
- if (true){
-  var a = 10;
- var b = 30;
- let c = 40;
-  return [a,b];
- }
-  console.log(abcd());
- console.log(b);
+
+//  golbal scope 
+
+//  any identifer is declared using by using var keywork can ba accessed anywhere in the progeram;
+//  var b = 20;
+//  if (true){
+//   var a = 10;
+//  var b = 30;
+//  let c = 40;
+//   return [a,b];
+//  }
+//   console.log(abcd());
+//  console.log(b);
+
+
+// document.write(typeof typeof typeof 2 +"1");
+// document.write(typeof typeof 2);
+// document.write(typeof typeof "sharuk" - 2);
+// document.write(typeof typeof 384n - 342);
+// console.log(typeof string);
+console.log(typeof 872n + typeof "hii");
+console.log(typeof 872n + typeof 2);
+console.log(typeof sharuk + typeof 89);
+console.log(typeof "sharuk" + typeof 89);
+console.log(typeof typeof 2 + typeof "sharuk");
+console.log(typeof typeof typeof undefined + typeof "sharuk");
+console.log(typeof typeof string - typeof "sharukh");
+console.log(typeof (typeof (null + 1) - typeof (undefined + "test")));
