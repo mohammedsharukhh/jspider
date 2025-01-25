7c404d47255ec5c26719bbb6aa3f7a957693960a
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-01-25 05:07:09 +0000

    worked on js

--- Jspiders Class/javacript/app.js
+++ Jspiders Class/javacript/app.js
@@ -1,3 +1,42 @@
+document.write(typeof 2+3);
+document.write(typeof Boolean);
+document.write(typeof Symbol());
+document.write(typeof null);
+document.write(typeof String);
+document.write(typeof 2 + Boolean());
+document.write(typeof Boolean());
+
+// practice questions
+
+document.write(2+2);
+document.writeln(typeof 2 +"2");
+document.write(typeof 1 * "2");
+document.write(typeof true + 2);
+document.write(typeof true * 2);
+document.write(2-2);
+document.write(typeof 2 - "2");
+document.write(typeof false - 2);
+document.write(typeof true + 2);
+document.write(typeof "2" - 4);
+ document.write(typeof 0 + 1);
+document.write(typeof null + null);
+document.write(typeof null - 45);
+document.write(typeof null + "3");
+document.write(typeof null + 5);
+document.write(46 + "54");
+document.write(31 - "hii");
+document.write(31 - "02");
+document.write(5 * "2");
+document.write("2" / 2);
+document.write(23 + "23");
+document.write(10 + "hii");
+document.write(typeof Boolean());
+document.write(typeof 2 - 2);
+document.write(typeof NaN);
+let a = "hii";
+document.write(isNaN(a)); 
+a = 10;
+document.write(isNaN(a));
  document.write(typeof 2+3);
  document.write(typeof Boolean);
  document.write(typeof Symbol());
@@ -98,8 +137,7 @@ golbal scope;
 script scope;
 block scope;
 local scope/ function scope;
-
-
+ -----------------------------------------
  golbal scope 
 
  any identifer is declared using by using var keywork can ba accessed anywhere in the progeram;
@@ -112,25 +150,53 @@ local scope/ function scope;
  }
   console.log(abcd());
  console.log(b);
-
-
-document.write(typeof typeof typeof 2 +"1");
-document.write(typeof typeof 2);
-document.write(typeof typeof "sharuk" - 2);
-document.write(typeof typeof 384n - 342);
-console.log(typeof string);
-console.log(typeof 872n + typeof "hii");
-console.log(typeof 872n + typeof 2);
-console.log(typeof sharuk + typeof 89);
-console.log(typeof "sharuk" + typeof 89);
-console.log(typeof typeof 2 + typeof "sharuk");
-console.log(typeof typeof typeof undefined + typeof "sharuk");
-console.log(typeof typeof string - typeof "sharukh");
-console.log(typeof (typeof (null + 1) - typeof (undefined + "test")));
-console.log(typeof undefined );// undefind is not a function
-console.log(typeof undefined +"test");
-
-console.log(typeof(typeof(null + 1)));
-
-console.log(typeof(string-undefinedtest);
-console.log(Number-String);
+console.log(typeof(typeof(typeof(undefined+"hello")* typeof(4+4))));
+console.log(typeof(typeof(typeof(5 + "5") * typeof(3 - "3"))));
+console.log(typeof(typeof(typeof("5" + 5) * typeof("6" - 6))));
+console.log(typeof(typeof(typeof(10 / "2") + typeof(7 + "1"))));
+console.log(typeof(typeof(typeof(null + "world") * typeof(undefined - 1))));
+console.log(typeof(typeof(typeof("hello" + "world") * typeof(2 * "2"))));
+console.log(typeof(typeof(typeof("8" / 2) - typeof("4" * "3"))));
+console.log(typeof(typeof(typeof(3 + "hello") * typeof("text" + 1))));
+console.log(typeof(typeof(typeof(5 * "3") / typeof("10" - 5))));
+console.log(typeof(typeof(typeof("abc" - 1) * typeof(4 / "2"))));
+console.log(typeof(typeof(typeof("text" + "value") - typeof(null + 1))));
+console.log(typeof(typeof(typeof(4 + "test") * typeof("value" * 2))));
+console.log(typeof(typeof(typeof("hello" + "world") / typeof(3 - "3"))));
+console.log(typeof(typeof(typeof("number" - 1) + typeof("4" / "2"))));
+console.log(typeof(typeof(typeof(null + "null") * typeof("7" * 2))));
+console.log(typeof(typeof(typeof("undefined" + "test") - typeof("5" * 3))));
+console.log(typeof(typeof(typeof("2" * 3) / typeof("value" + 1))));
+console.log(typeof(typeof(typeof(undefined + "type") * typeof("8" - 4))));
+console.log(typeof(typeof(typeof("text" + "undefined") / typeof(7 + "3"))));
+console.log(typeof(typeof(typeof("5" * 2) - typeof("3" + "value"))));
+console.log(typeof(typeof(typeof(2 / "1") * typeof("null" - "text"))));
+console.log(typeof(typeof(typeof(5 + "5") + typeof("world" * 3))));
+console.log(typeof(typeof(typeof("text" + 2) * typeof("8" / 4))));
+console.log(typeof(typeof(typeof(7 - "4") + typeof("type" * 2))));
+console.log(typeof(typeof(typeof("null" + "value") / typeof(3 * "3"))));
+console.log(typeof(typeof(typeof("string" - 5) * typeof(4 + "5"))));
+console.log(typeof(typeof(typeof(null + "hello") - typeof("9" * 2))));
+console.log(typeof(typeof(typeof("hello" * 2) + typeof("5" + "value"))));
+console.log(typeof(typeof(typeof(10 * "5") - typeof("2" + 2))));
+console.log(typeof(typeof(typeof("text" + "null") / typeof(7 - 3))));
+console.log(typeof(typeof(typeof(4 + "type") + typeof("text" * 4))));
+console.log(typeof(typeof(typeof(null + "world") * typeof("6" / "2"))));
+console.log(typeof(typeof(typeof("null" + "value") - typeof("7" * 3))));
+console.log(typeof(typeof(typeof("4" * 5) + typeof("string" - 1))));
+console.log(typeof(typeof(typeof("text" + "test") * typeof(3 / "3"))));
+console.log(typeof(typeof(typeof("value" - 4) + typeof(8 + "4"))));
+console.log(typeof(typeof(typeof(null + "null") / typeof("5" + 1))));
+console.log(typeof(typeof(typeof("2" + "test") - typeof("value" * 5))));
+console.log(typeof(typeof(typeof(undefined + "type") * typeof(10 + "3"))));
+console.log(typeof(typeof(typeof("hello" + "world") / typeof(6 - "3"))));
+console.log(typeof(typeof(typeof("text" + 5) + typeof("null" * 4))));
+console.log(typeof(typeof(typeof(4 / "2") * typeof("value" - "text"))));
+console.log(typeof(typeof(typeof("null" + "hello") + typeof(7 + "2"))));
+console.log(typeof(typeof(typeof("type" + "null") * typeof(2 / "4"))));
+console.log(typeof(typeof(typeof(5 + "test") - typeof("3" * 2))));
+console.log(typeof(typeof(typeof("7" / 2) + typeof("string" + 1))));
+console.log(typeof(typeof(typeof("text" - 4) * typeof(8 + "9"))));
+console.log(typeof(typeof(typeof("null" + 1) / typeof(3 + "value"))));
+console.log(typeof(typeof(typeof("world" + "hello") - typeof("4" * 3))));
+console.log(typeof(typeof(typeof(undefined + "null") * typeof("2" - 5))));
