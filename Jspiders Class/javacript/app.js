49adbc5294f0576520ded58ad314e63170b8d08d
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-01-27 06:52:52 +0000

    js function class in jspider

--- Jspiders Class/javacript/app.js
+++ Jspiders Class/javacript/app.js
@@ -1,296 +1,314 @@
-document.write(typeof 2+3);
-document.write(typeof Boolean);
-document.write(typeof Symbol());
-document.write(typeof String);
- document.write (typeof String["jhsdfhbsdhfb"]);
-document.write (typeof(String));
-document.write(typeof 2 + Boolean());
-document.write(typeof Boolean());
+// document.write(typeof 2+3);
+// document.write(typeof Boolean);
+// document.write(typeof Symbol());
+// document.write(typeof String);
+//  document.write (typeof String["jhsdfhbsdhfb"]);
+// document.write (typeof(String));
+// document.write(typeof 2 + Boolean());
+// document.write(typeof Boolean());
 
-// practice questions
+// // practice questions
 
-document.write(2+2);
-document.writeln(typeof 2 +"2");
-document.write(typeof 1 * "2");
-document.write(typeof true + 2);
-document.write(typeof true * 2);
-document.write(2-2);
-document.write(typeof 2 - "2");
-document.write(typeof false - 2);
-document.write(typeof true + 2);
-document.write(typeof "2" - 4);
- document.write(typeof 0 + 1);
-document.write(typeof null + null);
-document.write(typeof null - 45);
-document.write(typeof null + "3");
-document.write(typeof null + 5);
-document.write(46 + "54");
-document.write(31 - "hii");
-document.write(31 - "02");
-document.write(5 * "2");
-document.write("2" / 2);
-document.write(23 + "23");
-document.write(10 + "hii");
-document.write(typeof Boolean());
-document.write(typeof 2 - 2);
-document.write(typeof NaN);
-let a = "hii";
-document.write(isNaN(a)); 
-a = 10;
-document.write(isNaN(a));
- document.write(typeof 2+3);
- document.write(typeof Boolean);
- document.write(typeof Symbol());
- document.write(typeof null);
- document.write(typeof String);
- document.write(typeof 2 + Boolean());
- document.write(typeof Boolean());
+// document.write(2+2);
+// document.writeln(typeof 2 +"2");
+// document.write(typeof 1 * "2");
+// document.write(typeof true + 2);
+// document.write(typeof true * 2);
+// document.write(2-2);
+// document.write(typeof 2 - "2");
+// document.write(typeof false - 2);
+// document.write(typeof true + 2);
+// document.write(typeof "2" - 4);
+//  document.write(typeof 0 + 1);
+// document.write(typeof null + null);
+// document.write(typeof null - 45);
+// document.write(typeof null + "3");
+// document.write(typeof null + 5);
+// document.write(46 + "54");
+// document.write(31 - "hii");
+// document.write(31 - "02");
+// document.write(5 * "2");
+// document.write("2" / 2);
+// document.write(23 + "23");
+// document.write(10 + "hii");
+// document.write(typeof Boolean());
+// document.write(typeof 2 - 2);
+// document.write(typeof NaN);
+// let a = "hii";
+// document.write(isNaN(a)); 
+// a = 10;
+// document.write(isNaN(a));
+//  document.write(typeof 2+3);
+//  document.write(typeof Boolean);
+//  document.write(typeof Symbol());
+//  document.write(typeof null);
+//  document.write(typeof String);
+//  document.write(typeof 2 + Boolean());
+//  document.write(typeof Boolean());
 
-  practice questions
+//   practice questions
 
- document.write(2+2);
- document.writeln(typeof 2 - 2);
- document.write(typeof 1 * "2");
- document.write(typeof true + 2);
- document.write(typeof true * 2);
- document.write(2-2);
- document.write(typeof 2 - "2");
- document.write(typeof false - 2);
- document.write(typeof true + 2);
- document.write(typeof "2" - 4);
-  document.write(typeof 0 + 1);
- document.write(null);
- document.write(typeof null - 45);
- document.write(typeof null + "3");
- document.write(typeof null + 5);
- document.write(46 + "54");
- document.write(31 - "hii");
- document.write(31 - "02");
- document.write(5 * "2");
- document.write(typeof "/" + /);
- document.write(23 + "23");
- document.write(/10 + "hii");
- document.write(typeof Boolean());
- document.write(typeof 2 - 2);
- document.write(typeof NaN);
- let a = "hii";
- document.write(isNaN(a)); 
- a = 10;
- document.write(isNaN(a));
- document.write(typeof NaN);
+//  document.write(2+2);
+//  document.writeln(typeof 2 - 2);
+//  document.write(typeof 1 * "2");
+//  document.write(typeof true + 2);
+//  document.write(typeof true * 2);
+//  document.write(2-2);
+//  document.write(typeof 2 - "2");
+//  document.write(typeof false - 2);
+//  document.write(typeof true + 2);
+//  document.write(typeof "2" - 4);
+//   document.write(typeof 0 + 1);
+//  document.write(null);
+//  document.write(typeof null - 45);
+//  document.write(typeof null + "3");
+//  document.write(typeof null + 5);
+//  document.write(46 + "54");
+//  document.write(31 - "hii");
+//  document.write(31 - "02");
+//  document.write(5 * "2");
+//  document.write(typeof "/" + /);
+//  document.write(23 + "23");
+//  document.write(/10 + "hii");
+//  document.write(typeof Boolean());
+//  document.write(typeof 2 - 2);
+//  document.write(typeof NaN);
+//  let a = "hii";
+//  document.write(isNaN(a)); 
+//  a = 10;
+//  document.write(isNaN(a));
+//  document.write(typeof NaN);
 
- console.log("Hello World");
- document.write("Hello World");
- confirm("Are you sure?");
- alert("Hello World");
- prompt("Enter your name");
+//  console.log("Hello World");
+//  document.write("Hello World");
+//  confirm("Are you sure?");
+//  alert("Hello World");
+//  prompt("Enter your name");
 
- let a = 10;
- document.write(typeof a);
- a = "hii";
- document.write(typeof a);
- a = true;
- document.write(typeof a);
- let a= null;
- document.write(typeof a);
- a = undefined;
- document.write(typeof a);
- a = Symbol();
- document.write(typeof a);
- a = 12784812964172461729437465456547567567246n;
- document.write(typeof a);
- a = 10.5;
- document.write(typeof a);
+//  let a = 10;
+//  document.write(typeof a);
+//  a = "hii";
+//  document.write(typeof a);
+//  a = true;
+//  document.write(typeof a);
+//  let a= null;
+//  document.write(typeof a);
+//  a = undefined;
+//  document.write(typeof a);
+//  a = Symbol();
+//  document.write(typeof a);
+//  a = 12784812964172461729437465456547567567246n;
+//  document.write(typeof a);
+//  a = 10.5;
+//  document.write(typeof a);
 
- var a = "hii";
- console.log(a);
- a[0] = "bye";
- console.log(a);
+//  var a = "hii";
+//  console.log(a);
+//  a[0] = "bye";
+//  console.log(a);
 
- var a = [10,20,30,40,50];
- console.log(a);
- a[0] = 100;
- console.log(a); 
-
-
- console.log(a);
- var a = 10;
- console.log(a);
-
- console.log(a);
- let a = 10;
- console.log(a);
-
- document.write(1+2);
- document.write(1-"2");
-
- let number1 = 10(prompt("Please enter a number"));
- let number2 = Number(prompt("Please enter another number"));
- document.write(`The sum of ${number1} and ${number2} is ${number1 + number2}`);
-
- var a=10;
- var b=20;
- document.write(a+b);
- document.write(a);
+//  var a = [10,20,30,40,50];
+//  console.log(a);
+//  a[0] = 100;
+//  console.log(a); 
 
 
-golbal scope;
-script scope;
-block scope;
-local scope/ function scope;
- -----------------------------------------
- golbal scope 
+//  console.log(a);
+//  var a = 10;
+//  console.log(a);
 
- any identifer is declared using by using var keywork can ba accessed anywhere in the progeram;
- var b = 20;
- if (true){
-  var a = 10;
- var b = 30;
- let c = 40;
-  return [a,b];
- }
-  console.log(abcd());
- console.log(b);
-console.log(typeof(typeof(typeof(undefined+"hello")* typeof(4+4))));
-console.log(typeof(typeof(typeof(5 + "5") * typeof(3 - "3"))));
-console.log(typeof(typeof(typeof("5" + 5) * typeof("6" - 6))));
-console.log(typeof(typeof(typeof(10 / "2") + typeof(7 + "1"))));
-console.log(typeof(typeof(typeof(null + "world") * typeof(undefined - 1))));
-console.log(typeof(typeof(typeof("hello" + "world") * typeof(2 * "2"))));
-console.log(typeof(typeof(typeof("8" / 2) - typeof("4" * "3"))));
-console.log(typeof(typeof(typeof(3 + "hello") * typeof("text" + 1))));
-console.log(typeof(typeof(typeof(5 * "3") / typeof("10" - 5))));
-console.log(typeof(typeof(typeof("abc" - 1) * typeof(4 / "2"))));
-console.log(typeof(typeof(typeof("text" + "value") - typeof(null + 1))));
-console.log(typeof(typeof(typeof(4 + "test") * typeof("value" * 2))));
-console.log(typeof(typeof(typeof("hello" + "world") / typeof(3 - "3"))));
-console.log(typeof(typeof(typeof("number" - 1) + typeof("4" / "2"))));
-console.log(typeof(typeof(typeof(null + "null") * typeof("7" * 2))));
-console.log(typeof(typeof(typeof("undefined" + "test") - typeof("5" * 3))));
-console.log(typeof(typeof(typeof("2" * 3) / typeof("value" + 1))));
-console.log(typeof(typeof(typeof(undefined + "type") * typeof("8" - 4))));
-console.log(typeof(typeof(typeof("text" + "undefined") / typeof(7 + "3"))));
-console.log(typeof(typeof(typeof("5" * 2) - typeof("3" + "value"))));
-console.log(typeof(typeof(typeof(2 / "1") * typeof("null" - "text"))));
-console.log(typeof(typeof(typeof(5 + "5") + typeof("world" * 3))));
-console.log(typeof(typeof(typeof("text" + 2) * typeof("8" / 4))));
-console.log(typeof(typeof(typeof(7 - "4") + typeof("type" * 2))));
-console.log(typeof(typeof(typeof("null" + "value") / typeof(3 * "3"))));
-console.log(typeof(typeof(typeof("string" - 5) * typeof(4 + "5"))));
-console.log(typeof(typeof(typeof(null + "hello") - typeof("9" * 2))));
-console.log(typeof(typeof(typeof("hello" * 2) + typeof("5" + "value"))));
-console.log(typeof(typeof(typeof(10 * "5") - typeof("2" + 2))));
-console.log(typeof(typeof(typeof("text" + "null") / typeof(7 - 3))));
-console.log(typeof(typeof(typeof(4 + "type") + typeof("text" * 4))));
-console.log(typeof(typeof(typeof(null + "world") * typeof("6" / "2"))));
-console.log(typeof(typeof(typeof("null" + "value") - typeof("7" * 3))));
-console.log(typeof(typeof(typeof("4" * 5) + typeof("string" - 1))));
-console.log(typeof(typeof(typeof("text" + "test") * typeof(3 / "3"))));
-console.log(typeof(typeof(typeof("value" - 4) + typeof(8 + "4"))));
-console.log(typeof(typeof(typeof(null + "null") / typeof("5" + 1))));
-console.log(typeof(typeof(typeof("2" + "test") - typeof("value" * 5))));
-console.log(typeof(typeof(typeof(undefined + "type") * typeof(10 + "3"))));
-console.log(typeof(typeof(typeof("hello" + "world") / typeof(6 - "3"))));
-console.log(typeof(typeof(typeof("text" + 5) + typeof("null" * 4))));
-console.log(typeof(typeof(typeof(4 / "2") * typeof("value" - "text"))));
-console.log(typeof(typeof(typeof("null" + "hello") + typeof(7 + "2"))));
-console.log(typeof(typeof(typeof("type" + "null") * typeof(2 / "4"))));
-console.log(typeof(typeof(typeof(5 + "test") - typeof("3" * 2))));
-console.log(typeof(typeof(typeof("7" / 2) + typeof("string" + 1))));
-console.log(typeof(typeof(typeof("text" - 4) * typeof(8 + "9"))));
-console.log(typeof(typeof(typeof("null" + 1) / typeof(3 + "value"))));
-console.log(typeof(typeof(typeof("world" + "hello") - typeof("4" * 3))));
-console.log(typeof(typeof(typeof(undefined + "null") * typeof("2" - 5))));
-let ak = 20;
-let bk = 20;
-console.log( ak !== bk);
+//  console.log(a);
+//  let a = 10;
+//  console.log(a);
+
+//  document.write(1+2);
+//  document.write(1-"2");
+
+//  let number1 = 10(prompt("Please enter a number"));
+//  let number2 = Number(prompt("Please enter another number"));
+//  document.write(`The sum of ${number1} and ${number2} is ${number1 + number2}`);
+
+//  var a=10;
+//  var b=20;
+//  document.write(a+b);
+//  document.write(a);
 
 
-let x = 10;
-let y = 15;
-console.log(x !== y);
+// golbal scope;
+// script scope;
+// block scope;
+// local scope/ function scope;
+//  -----------------------------------------
+//  golbal scope 
 
-let a = "hello";
-let b = "world";
-console.log(a == b);
-
-let num1 = 5;
-let num2 = "5";
-console.log(num1 === num2);
-
-let age1 = 25;
-let age2 = 20;
-console.log(age1 > age2);
-
-let price1 = 100;
-let price2 = 150;
-console.log(price1 < price2);
-
-let score1 = 80;
-let score2 = 80;
-console.log(score1 >= score2);
-
-let height1 = 6.2;
-let height2 = 6.1;
-console.log(height1 <= height2);
-
-let isRaining = true;
-let isSnowing = false;
-console.log(isRaining != isSnowing);
-
-let apples = 10;
-let bananas = 20;
-console.log(apples < bananas);
-
-let oranges = 15;
-let grapes = 15;
-console.log(oranges == grapes);
-
-let speed1 = 60;
-let speed2 = 80;
-console.log(speed1 <= speed2);
-
-let temp1 = 35;
-let temp2 = 35;
-console.log(temp1 >= temp2);
-
-let length1 = 10.5;
-let length2 = 12.5;
-console.log(length1 < length2);
-
-let width1 = 7.2;
-let width2 = 7.2;
-console.log(width1 === width2);
-
-let hours1 = 8;
-let hours2 = 6;
-console.log(hours1 > hours2);
-
-let days1 = 5;
-let days2 = 10;
-console.log(days1 != days2);
-
-let val1 = null;
-let val2 = undefined;
-console.log(val1 !== val2);
-
-let numA = 0;
-let numB = "0";
-console.log(numA == numB);
-
-let scoreA = 100;
-let scoreB = 90;
-console.log(scoreA > scoreB);
-
-let weight1 = 70;
-let weight2 = 70;
-console.log(weight1 <= weight2);
+//  any identifer is declared using by using var keywork can ba accessed anywhere in the progeram;
+//  var b = 20;
+//  if (true){
+//   var a = 10;
+//  var b = 30;
+//  let c = 40;
+//   return [a,b];
+//  }
+//   console.log(abcd());
+//  console.log(b);
+// console.log(typeof(typeof(typeof(undefined+"hello")* typeof(4+4))));
+// console.log(typeof(typeof(typeof(5 + "5") * typeof(3 - "3"))));
+// console.log(typeof(typeof(typeof("5" + 5) * typeof("6" - 6))));
+// console.log(typeof(typeof(typeof(10 / "2") + typeof(7 + "1"))));
+// console.log(typeof(typeof(typeof(null + "world") * typeof(undefined - 1))));
+// console.log(typeof(typeof(typeof("hello" + "world") * typeof(2 * "2"))));
+// console.log(typeof(typeof(typeof("8" / 2) - typeof("4" * "3"))));
+// console.log(typeof(typeof(typeof(3 + "hello") * typeof("text" + 1))));
+// console.log(typeof(typeof(typeof(5 * "3") / typeof("10" - 5))));
+// console.log(typeof(typeof(typeof("abc" - 1) * typeof(4 / "2"))));
+// console.log(typeof(typeof(typeof("text" + "value") - typeof(null + 1))));
+// console.log(typeof(typeof(typeof(4 + "test") * typeof("value" * 2))));
+// console.log(typeof(typeof(typeof("hello" + "world") / typeof(3 - "3"))));
+// console.log(typeof(typeof(typeof("number" - 1) + typeof("4" / "2"))));
+// console.log(typeof(typeof(typeof(null + "null") * typeof("7" * 2))));
+// console.log(typeof(typeof(typeof("undefined" + "test") - typeof("5" * 3))));
+// console.log(typeof(typeof(typeof("2" * 3) / typeof("value" + 1))));
+// console.log(typeof(typeof(typeof(undefined + "type") * typeof("8" - 4))));
+// console.log(typeof(typeof(typeof("text" + "undefined") / typeof(7 + "3"))));
+// console.log(typeof(typeof(typeof("5" * 2) - typeof("3" + "value"))));
+// console.log(typeof(typeof(typeof(2 / "1") * typeof("null" - "text"))));
+// console.log(typeof(typeof(typeof(5 + "5") + typeof("world" * 3))));
+// console.log(typeof(typeof(typeof("text" + 2) * typeof("8" / 4))));
+// console.log(typeof(typeof(typeof(7 - "4") + typeof("type" * 2))));
+// console.log(typeof(typeof(typeof("null" + "value") / typeof(3 * "3"))));
+// console.log(typeof(typeof(typeof("string" - 5) * typeof(4 + "5"))));
+// console.log(typeof(typeof(typeof(null + "hello") - typeof("9" * 2))));
+// console.log(typeof(typeof(typeof("hello" * 2) + typeof("5" + "value"))));
+// console.log(typeof(typeof(typeof(10 * "5") - typeof("2" + 2))));
+// console.log(typeof(typeof(typeof("text" + "null") / typeof(7 - 3))));
+// console.log(typeof(typeof(typeof(4 + "type") + typeof("text" * 4))));
+// console.log(typeof(typeof(typeof(null + "world") * typeof("6" / "2"))));
+// console.log(typeof(typeof(typeof("null" + "value") - typeof("7" * 3))));
+// console.log(typeof(typeof(typeof("4" * 5) + typeof("string" - 1))));
+// console.log(typeof(typeof(typeof("text" + "test") * typeof(3 / "3"))));
+// console.log(typeof(typeof(typeof("value" - 4) + typeof(8 + "4"))));
+// console.log(typeof(typeof(typeof(null + "null") / typeof("5" + 1))));
+// console.log(typeof(typeof(typeof("2" + "test") - typeof("value" * 5))));
+// console.log(typeof(typeof(typeof(undefined + "type") * typeof(10 + "3"))));
+// console.log(typeof(typeof(typeof("hello" + "world") / typeof(6 - "3"))));
+// console.log(typeof(typeof(typeof("text" + 5) + typeof("null" * 4))));
+// console.log(typeof(typeof(typeof(4 / "2") * typeof("value" - "text"))));
+// console.log(typeof(typeof(typeof("null" + "hello") + typeof(7 + "2"))));
+// console.log(typeof(typeof(typeof("type" + "null") * typeof(2 / "4"))));
+// console.log(typeof(typeof(typeof(5 + "test") - typeof("3" * 2))));
+// console.log(typeof(typeof(typeof("7" / 2) + typeof("string" + 1))));
+// console.log(typeof(typeof(typeof("text" - 4) * typeof(8 + "9"))));
+// console.log(typeof(typeof(typeof("null" + 1) / typeof(3 + "value"))));
+// console.log(typeof(typeof(typeof("world" + "hello") - typeof("4" * 3))));
+// console.log(typeof(typeof(typeof(undefined + "null") * typeof("2" - 5))));
+// let ak = 20;
+// let bk = 20;
+// console.log( ak !== bk);
 
 
-let a = 20;
-let result = (a == 30)? ("Its equal"):("No its not equal");
-console.log(result);
+// let x = 10;
+// let y = 15;
+// console.log(x !== y);
 
+// let a = "hello";
+// let b = "world";
+// console.log(a == b);
+
+// let num1 = 5;
+// let num2 = "5";
+// console.log(num1 === num2);
+
+// let age1 = 25;
+// let age2 = 20;
+// console.log(age1 > age2);
+
+// let price1 = 100;
+// let price2 = 150;
+// console.log(price1 < price2);
+
+// let score1 = 80;
+// let score2 = 80;
+// console.log(score1 >= score2);
+
+// let height1 = 6.2;
+// let height2 = 6.1;
+// console.log(height1 <= height2);
+
+// let isRaining = true;
+// let isSnowing = false;
+// console.log(isRaining != isSnowing);
+
+// let apples = 10;
+// let bananas = 20;
+// console.log(apples < bananas);
+
+// let oranges = 15;
+// let grapes = 15;
+// console.log(oranges == grapes);
+
+// let speed1 = 60;
+// let speed2 = 80;
+// console.log(speed1 <= speed2);
+
+// let temp1 = 35;
+// let temp2 = 35;
+// console.log(temp1 >= temp2);
+
+// let length1 = 10.5;
+// let length2 = 12.5;
+// console.log(length1 < length2);
+
+// let width1 = 7.2;
+// let width2 = 7.2;
+// console.log(width1 === width2);
+
+// let hours1 = 8;
+// let hours2 = 6;
+// console.log(hours1 > hours2);
+
+// let days1 = 5;
+// let days2 = 10;
+// console.log(days1 != days2);
+
+// let val1 = null;
+// let val2 = undefined;
+// console.log(val1 !== val2);
+
+// let numA = 0;
+// let numB = "0";
+// console.log(numA == numB);
+
+// let scoreA = 100;
+// let scoreB = 90;
+// console.log(scoreA > scoreB);
+
+// let weight1 = 70;
+// let weight2 = 70;
+// console.log(weight1 <= weight2);
+
+
+// let a = 20;
+// let result = (a == 30)? ("Its equal"):("No its not equal");
+// console.log(result);
+
+
+
+function sharuk(){
+    console.log("hii");
+    function zareena(){
+        console.log("hello")
+        function fayaz(){
+            console.log("hey")
+            function mohammedali(){
+                console.log("yes")
+            }
+            return mohammedali();
+        }
+        fayaz();
+        }
+        zareena();
+}
+sharuk();
 
 
 
