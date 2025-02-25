a038ee4f9dc47afe4e83982f4f6c41d7b69c6dba
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-02-25 05:17:58 +0000

    updated a bit on calculator

---
+++ Assignment-1/calculat/app.js
@@ -0,0 +1,25 @@
+const inputField = document.getElementById('input');
+const buttons = document.querySelectorAll('.btn, .btn1');
+
+let currentInput = '';
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonText = button.innerText;
+
+        if (buttonText === '=') {
+            try {
+                currentInput = eval(currentInput).toString();
+            } catch {
+                currentInput = 'Error';
+            }
+        } else if (buttonText === 'C') {
+            currentInput = '';
+        } else {
+            currentInput += buttonText;
+        }
+
+        inputField.value = currentInput;
+    });
+});
