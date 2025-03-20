869029146b31c107cf450379f4a01e8234976528
Mohammed Sharuk M S <149393894+mohammedsharukhh@users.noreply.github.com>
2025-03-20 17:30:48 +0000

    uploaded js image functioning project in project folder

---
+++ Projects/image_over/app.js
@@ -0,0 +1,20 @@
+document.getElementById("nature").addEventListener("click", () => {
+    let container = document.getElementById('container')
+    container.innerHTML = `
+    <img src = './nature.png' class = "container"></img>
+    `
+  });
+
+  document.getElementById("Person").addEventListener("click", () => {
+    let container = document.getElementById('container')
+    container.innerHTML = `
+    <img src = './Person.png' class = "container"></img>
+    `
+  });
+
+  document.getElementById("sky").addEventListener("click", () => {
+    let container = document.getElementById('container')
+    container.innerHTML = `
+    <img src = './sky.png' class = "container"></img>
+    `
+  });
